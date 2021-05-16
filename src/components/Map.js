fc8bd197172ed6aef434497c7baa72855a80e7e2
Gaurav Khatri <gauravkhatri2698@gmail.com>
2021-05-16 21:36:01 +0530

    Covid dashboard

---
+++ src/components/Map.js
@@ -0,0 +1,69 @@
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
+
+import { useConfigureLeaflet, useMapServices, useRefEffect } from "hooks";
+import { isDomAvailable } from "lib/util";
+
+const Map = (props) => {
+  const {
+    children,
+    className,
+    defaultBaseMap = "OpenStreetMap",
+    MapEffect,
+    ...rest
+  } = props;
+
+  const mapRef = useRef();
+
+  useConfigureLeaflet();
+
+  useRefEffect({
+    ref: mapRef,
+    effect: MapEffect,
+  });
+
+  const services = useMapServices({
+    names: ["OpenStreetMap"],
+  });
+  const basemap = services.find((service) => service.name === defaultBaseMap);
+
+  let mapClassName = `map`;
+
+  if (className) {
+    mapClassName = `${mapClassName} ${className}`;
+  }
+
+  if (!isDomAvailable()) {
+    return (
+      <div className={mapClassName}>
+        <p className="map-loading">Loading map...</p>
+      </div>
+    );
+  }
+
+  const mapSettings = {
+    className: "map-base",
+    zoomControl: false,
+    ...rest,
+  };
+
+  return (
+    <div className={mapClassName}>
+      <MapContainer innerRef={mapRef} {...mapSettings}>
+        {children}
+        {basemap && <TileLayer {...basemap} />}
+        <ZoomControl position="bottomright" />
+      </MapContainer>
+    </div>
+  );
+};
+
+Map.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  defaultBaseMap: PropTypes.string,
+  mapEffect: PropTypes.func,
+};
+
+export default Map;
---
+++ src/hooks/useTracker.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_HOST = 'https://corona.lmao.ninja/v2';
+
+const ENDPOINTS = [
+  {
+    id: 'all',
+    path: '/all',
+    isDefault: true
+  },
+  {
+    id: 'countries',
+    path: '/countries'
+  }
+]
+
+const defaultState = {
+  data: null,
+  state: 'ready'
+}
+
+const useTracker = ({ api = 'all' }) => {
+
+  const [tracker = {}, updateTracker] = useState(defaultState);
+
+  const fetchTracker = React.useCallback(async () => {
+        let route = ENDPOINTS.find(({ id } = {}) => id === api);
+        let response;
+
+        if (!route) {
+          route = ENDPOINTS.find(({ isDefault } = {}) => !!isDefault);
+        }
+
+        try {
+          updateTracker((prev) => {
+            return {
+              ...prev,
+              state: 'loading'
+            }
+          });
+          response = await axios.get(`${API_HOST}${route.path}`);
+        } 
+        catch (e) {
+          updateTracker((prev) => {
+            return {
+              ...prev,
+              state: 'error',
+              error: e
+            }
+          });
+          return;
+        }
+
+        const { data } = response;
+
+        updateTracker((prev) => {
+            return {
+                ...prev,
+                state: 'ready',
+                data
+            };
+        });
+    },[api]);
+
+  useEffect(() => {
+    fetchTracker()
+  }, [api, fetchTracker]);
+  
+  return {
+    fetchTracker,
+    ...tracker
+  }
+};
+
+export default useTracker;
